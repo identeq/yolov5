088f1e42fcf0b71e53c70c50834cdd5b1e7999b2
Ashok Kumar Pant <ashokpant87@gmail.com>
2022-08-01 17:03:10 +0545

    Add yolov5 detector

---
+++ examples/__init__.py
@@ -0,0 +1,4 @@
+"""
+-- Created by: Ashok Kumar Pant
+-- Created on: 8/1/22
+"""
---
+++ yolov5/detector.py
@@ -0,0 +1,28 @@
+"""
+-- Created by: Ashok Kumar Pant
+-- Created on: 8/1/22
+"""
+from yolov5.models.common import DetectMultiBackend, AutoShapeV1
+from yolov5.utils.torch_utils import select_device
+
+
+class YoloV5Detector:
+    def __init__(self, model_name="yolov5s.pt", img_size=640):
+        self.img_size = img_size
+        self.device = select_device()
+        self.backend_model = DetectMultiBackend(model_name, device=self.device)
+        self.model = AutoShapeV1(self.backend_model).to(self.device)
+        self.labels = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+
+    def detect(self, image, thresh=0.25, iou_thres=0.45, classes=None, agnostic=False):
+        results = self.model(image, size=self.img_size, conf=thresh, iou=iou_thres, classes=classes, agnostic=agnostic)
+        boxes = []
+        confidences = []
+        class_ids = []
+        for i in range(len(results.xyxy[0])):
+            x0, y0, x1, y1, confidence, class_id = results.xyxy[0][i].cpu().numpy().astype(float)
+            x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
+            boxes.append([x0, y0, x1, y1])
+            confidences.append(float(confidence))
+            class_ids.append(int(class_id))
+        return boxes, class_ids, confidences
